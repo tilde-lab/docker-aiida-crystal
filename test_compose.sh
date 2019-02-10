56b77aaf28d005fb74fc2bf0661b41b3e98cde21
Andrey Sobolev <andrey.n.sobolev@gmail.com>
2019-02-10 21:59:51 +0500

    Fixed a bug with aiida repo not in volume

--- test_compose.sh
+++ test_compose.sh
@@ -4,6 +4,6 @@
 set -e
 
 ## I can now setup the computer etc.
-docker-compose exec --user aiida aiida /bin/bash -l -c "verdi run /home/aiida/code/aiida-crystal/examples/test_KMnF3.py"
-#docker-compose exec --user aiida aiida /bin/bash -l -c "verdi run /home/aiida/code/aiida-crystal/examples/test_MgO.py"
+#docker-compose exec --user aiida aiida /bin/bash -l -c "verdi run /home/aiida/code/aiida-crystal/examples/test_KMnF3.py"
+docker-compose exec --user aiida aiida /bin/bash -l -c "verdi run /home/aiida/code/aiida-crystal/examples/test_MgO_new.py"
 #docker-compose exec --user root db /bin/bash -l -c "pg_dump -U aiida -d aiida_default -h localhost -C --column-inserts" > dump.sql
