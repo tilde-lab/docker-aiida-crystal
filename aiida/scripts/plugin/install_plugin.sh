9f6066ab09a140478d8ef7be48fa923c3aac1b75
Andrey Sobolev <andrey.n.sobolev@gmail.com>
2019-07-30 13:58:29 +0500

    Changed working commit of aiida

--- aiida/scripts/plugin/install_plugin.sh
+++ aiida/scripts/plugin/install_plugin.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+export BRANCH=basisset
+
 exec /sbin/setuser aiida \
-pip install --user git+file:///home/aiida/code/aiida-crystal@master /home/aiida/code/mpds-aiida
+pip install --user git+file:///home/aiida/code/aiida-crystal@$BRANCH git+file:///home/aiida/code/mpds-aiida@$BRANCH
--- run_workflow.sh
+++ run_workflow.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-docker-compose exec --user aiida aiida /bin/bash -l -c "cd /home/aiida/code//mpds-aiida/mpds_aiida_workflows/tests/basis && verdi data crystal-basis uploadfamily --name=sto-3g --description=sto-3g"
+docker-compose exec --user aiida aiida /bin/bash -l -c "verdi data crystal createpredefined"
 docker-compose exec --user aiida aiida /bin/bash -l -c "verdi run /home/aiida/code/aiida-crystal/examples/test_MgO_MPDS.py"
