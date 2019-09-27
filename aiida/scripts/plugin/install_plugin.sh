49278ec65235f9d1b13a0d340adb36b5a2877dd4
Andrey Sobolev <andrey.n.sobolev@gmail.com>
2019-09-27 10:09:27 +0500

    Added basis family to Docker AiiDA instance

--- aiida/scripts/plugin/install_plugin.sh
+++ aiida/scripts/plugin/install_plugin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export BRANCH=basisset
+export BRANCH=aiida_beta
 
-exec /sbin/setuser aiida \
-pip install --user git+file:///home/aiida/code/aiida-crystal@$BRANCH git+file:///home/aiida/code/mpds-aiida@$BRANCH
+exec /sbin/setuser aiida pip install --user -e /home/aiida/code/aiida-crystal /home/aiida/code/mpds-aiida
+# pip install --user git+file:///home/aiida/code/aiida-crystal@$BRANCH git+file:///home/aiida/code/mpds-aiida@$BRANCH
--- run_workflow.sh
+++ run_workflow.sh
@@ -3,4 +3,5 @@
 set -e
 
 docker-compose exec --user aiida aiida /bin/bash -l -c "verdi data crystal createpredefined"
+docker-compose exec --user aiida aiida /bin/bash -l -c "verdi data crystal uploadfamily --name MINIMAL --path ~/code/aiida-crystal/aiida_crystal/tests/input_files/sto3g"
 docker-compose exec --user aiida aiida /bin/bash -l -c "verdi run /home/aiida/code/aiida-crystal/examples/test_MgO_MPDS.py"
